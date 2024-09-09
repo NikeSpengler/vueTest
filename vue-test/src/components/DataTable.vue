b1f6b90c6d54ca34b8fc56dc695a514e3b85f5bf
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-09-09 16:05:08 +0200

    added new file DataTable and a JSON file

---
+++ vue-test/src/components/DataTable.vue
@@ -0,0 +1,38 @@
+<script>
+import aktivBo from '../aktivbo.json'
+
+export default {
+  data() {
+    return { 
+      data: aktivBo 
+    }
+  }
+ }
+</script>
+
+
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Projektnamn</th>
+                    <th>Projektstatus</th>
+                    <th>Undersökningstyp</th>
+                    <th>Antal respondenter</th>
+                    <th>Svarsprocent</th>
+                    <th>Integration</th>
+                    <th>Startdatum</th>
+                    <th>Slutdatum</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in data" :key="item.id"> 
+                    <td>{{ item.project_name }} </td>
+                </tr>
+            </tbody>
+            
+        </table>
+    </div>
+ 
+</template>
